1242284c1a863b3339836b63bad8d609d59af5e7
Luis Moreira <luis010101work@outlook.com>
2023-11-17 05:56:54 -0300

    droga...

--- site/src/components/AdminBarraL/index.jsx
+++ site/src/components/AdminBarraL/index.jsx
@@ -25,37 +25,37 @@ export default function AdmBarraLateral(props){
         <nav className="all">
             <header className="title">
                 <div className="title-conteudo">
-                    <h1>Game</h1>
+                    <h1 class="animate__animated animate__flash">Game</h1>
                     <img src="/assets/images/GameSync/giphy-unscreen.gif" alt="" />
-                    <h1>Sync</h1>
+                    <h1 class="animate__animated animate__flash">Sync</h1>
                 </div>
             </header>
 
             <section className="opcoes">
                 <Link to='/admin/dashboard' className={`opcoes-z ${props.selecionado === 'dashboard' ? 'selecionado' : ''}`}>
                     <div className="opcoes-z">
-                        <h1>DASHBOARD</h1>
+                        <h1 class="animate__animated animate__backInLeft">DASHBOARD</h1>
                         <img src="/assets/images/adm/dashboard.png" alt="dashboard" />
                     </div>
                 </Link>
                 {/* Se a prop 'selecionado' for 'home', a classe 'selecionado' será aplicada */}
                 <Link to='/admin/addproduts' className={`opcoes-z ${SessionSelect({ opcoes: 'addproduts', selecionado: props.selecionado })}`}>
                     <div className="opcoes-z">
-                        <h1>ADD PRODUCTS</h1>
+                        <h1 class="animate__animated animate__backInLeft">ADD PRODUCTS</h1>
                         <img src="/assets/images/adm/add 2.png" alt="addproduts" />
                     </div>
                 </Link>
 
                 <Link to='' className={`opcoes-z ${props.selecionado === 'NEWS' ? 'selecionado' : ''}`}>
                     <div className="opcoes-z">
-                        <h1>NEWS</h1>
+                        <h1 class="animate__animated animate__backInLeft">NEWS</h1>
                         <img src="/assets/images/adm/newspaper.png" alt="" />
                     </div>
                 </Link>
 
                 <Link to='/admin/MudarProduto' className={`opcoes-z ${SessionSelect({ opcoes: 'MudarProduto', selecionado: props.selecionado })}`}>
                     <div className="opcoes-z">
-                        <h1>CHANGES</h1>
+                        <h1 class="animate__animated animate__backInLeft">CHANGES</h1>
                         <img src="/assets/images/adm/mudar.png" alt="MudarProduto" />
                     </div>
                 </Link>
@@ -63,7 +63,7 @@ export default function AdmBarraLateral(props){
 
             <footer className="sair">
                 <div className="sair-msm" onClick={sairLogin}>
-                    <h1>Log Out</h1>
+                    <h1 class="animate__animated animate__fadeIn">Log Out</h1>
                     <img src="/assets/images/adm/sair 1.png" alt="" />
                 </div>
             </footer>
